2587f43e791f94567c640fbbffdae58111a17d59
senolduyguuu <duygusenol00@gmail.com>
2024-06-13 14:22:46 +0300

    React router kuruldu

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+
+import ReactDOM from 'react-dom/client'
+import { RouterProvider } from 'react-router-dom'
+import router from "./store/indexStore.jsx"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+	<RouterProvider router={router} />
+
+
+)
---
+++ src/store/indexStore.jsx
@@ -0,0 +1,17 @@
+import {
+	createBrowserRouter,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: "ana sayfa",
+
+	},
+	{
+		path: "/welcome",
+		element: "Hoş geldiniz",
+
+	},
+]);
+export default router
